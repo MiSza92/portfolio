1135e0e291c9ae9d7e4bbca85aef362f15730bf0
MSZ <m.szabo@outlook.de>
2024-02-02 14:51:51 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./component/NavBar/NavBar";
 
 import Skills from "./component/pages/Skills";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./component/pages/Landing";
 import Projects from "./component/pages/Projects";
 import Contact from "./component/pages/Contact";
@@ -11,15 +11,13 @@ function App() {
   const location = useLocation();
   return (
     <>
-      <BrowserRouter basename="/portfolio">
-        <NavBar />
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }

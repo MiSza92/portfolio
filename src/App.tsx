6d81500a8dc47824e7286d90622d50c17cd7b443
MSZ <m.szabo@outlook.de>
2024-02-02 16:06:41 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Skills from "./component/pages/Skills";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./component/pages/Landing";
 import Projects from "./component/pages/Projects";
-import Contact from "./component/pages/Contact";
+//import Contact from "./component/pages/Contact";
 
 function App() {
   const location = useLocation();
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </>
   );
--- src/component/pages/Contact.tsx
+++ src/component/pages/Contact.tsx
@@ -1,49 +1,49 @@
-import "./ContactStyle.css";
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
+// import "./ContactStyle.css";
+// import { useRef } from "react";
+// import emailjs from "@emailjs/browser";
 
-function Contact() {
-  const form = useRef();
+// function Contact() {
+//   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+//   const sendEmail = (e) => {
+//     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_vz92nii",
-        "template_j3d75w8",
-        form.current,
-        "2sQOv_mJ49MHLUQ6L"
-      )
-      .then(
-        () => {
-          alert("Message sent successfully");
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-  return (
-    <div className="contactContainer">
-      <div className="contactForm">
-        <form ref={form} onSubmit={sendEmail}>
-          <input
-            type="text"
-            name="from_name"
-            id="name"
-            placeholder="Name"
-            required
-          />
+//     emailjs
+//       .sendForm(
+//         "service_vz92nii",
+//         "template_j3d75w8",
+//         form.current,
+//         "2sQOv_mJ49MHLUQ6L"
+//       )
+//       .then(
+//         () => {
+//           alert("Message sent successfully");
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
+//   return (
+//     <div className="contactContainer">
+//       <div className="contactForm">
+//         <form ref={form} onSubmit={sendEmail}>
+//           <input
+//             type="text"
+//             name="from_name"
+//             id="name"
+//             placeholder="Name"
+//             required
+//           />
 
-          <input type="email" name="from_email" placeholder="Email" required />
+//           <input type="email" name="from_email" placeholder="Email" required />
 
-          <textarea name="message" placeholder="Message" required />
-          <input type="submit" value="Send" className="btnSubmit" />
-        </form>
-      </div>
-    </div>
-  );
-}
+//           <textarea name="message" placeholder="Message" required />
+//           <input type="submit" value="Send" className="btnSubmit" />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Contact;
+// export default Contact;
